8a894acb13db4b7bc162b9b177050258b6bb02bb
Ardian Pratama <ardnpratm@gmail.com>
2026-03-25 10:11:53 +0700

    update sitemap[.]xml.ts

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/sitemap.xml')({
         `<?xml version="1.0" encoding="UTF-8"?>
           <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             <url>
-              <loc>https://ardian-pratama.vercel.app</loc>
+              <loc>https://ardian-pratama.vercel.app/</loc>
               <lastmod>2026-03-25</lastmod>
             </url>
             <url>
